9c52a289c033c6b3fb6c4d54068f98027812f50a
ramani-3528 <ramaneshwari3528@gmail.com>
2025-08-05 17:05:37 +0530

    after

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ accordions.forEach(button => {
     button.addEventListener("click", () => {
         const panel = button.nextElementSibling;
         const isOpen = panel.style.display === "block";
+
         document.querySelectorAll(".panel").forEach(p => p.style.display = "none");
-        panel.style.display = isOpen ? "none" : "block";
+        document.querySelectorAll(".accordion").forEach(btn => btn.classList.remove("active"));
+
+        if (!isOpen) {
+            panel.style.display = "block";
+            button.classList.add("active");
+        }
     });
 });
